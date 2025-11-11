d45b09b9026b5c3866a49d6b0d2947ee37b3148b
Cristian Arango <cristian.arango@dms.ms>
2025-11-11 10:24:24 -0500

    Actualiza DTO y cadena de conexión a base de datos
    Se modifica la propiedad `TaskStatusId` en `GetFilterTasksDTO.cs` para que tenga un valor predeterminado de `null`. Además, se actualiza la cadena de conexión `DefaultConnection` en `appsettings.json` para apuntar a una base de datos remota con nuevas credenciales.

--- task.core/DTOs/Tasks/GetFilterTasksDTO.cs
+++ task.core/DTOs/Tasks/GetFilterTasksDTO.cs
@@ -2,7 +2,7 @@
 {
     public class GetFilterTasksDTO
     {
-        public int? TaskStatusId { get; set; }
+        public int? TaskStatusId { get; set; } = null;
         public string? SearchTerm { get; set; }
         public int? PageSize { get; set; }
         public int? PageNumber { get; set; }
